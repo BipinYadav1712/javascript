b41c63f887e2c56c249fef629bbb7d76fac38477
BipinYadav1712 <bipinyadav1712@gmail.com>
2025-02-02 03:34:13 +0530

    variables const done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 144553
+let accountEmail = "bipinyadav1712@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountID = 2 not allowed
+accountEmail ="hitesh123@gmail.com"
+accountPassword = "789456123"
+accountCity ="gorakhpur"
+let accountState;
+// console.log(accountCity);
+/*
+var ka use hamesha avoid karna hai kyuki dikkat krta h ye blockscope aur funcitonal scope mein dikkat;
+*/
+console.table([accountEmail,accountID,accountPassword,accountCity,accountState])
