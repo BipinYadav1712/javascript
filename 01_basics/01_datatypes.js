02650916cfdd120695ef43b6b31b9a1fb6157a1c
BipinYadav1712 <bipinyadav1712@gmail.com>
2025-02-02 18:04:47 +0530

    data types basics done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all code as newer version
+
+// alert(3+3) // we are using node.js not browser  
+console.log( 3 + 3) //code readability should be high
+
+console.log("hitesh");
+
+
+let name ="bipin" // string datatypes
+let age = 18 // number
+let isLoggedIn = false //string
+
+
+// number =>2 to power 53 range 
+// bigINT
+// string => ""
+// bbolean = 'true' or 'false'
+// null => stand alone value  ye ek object bhi hai
+// undefined =>
+// symbol => uniqueness
+// upar vale primitive data types
+
+
+
+//object 
+
+console.log(typeof undefined)
