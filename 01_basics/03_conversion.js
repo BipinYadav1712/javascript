8fb430a617f715074d55501366aee76f3d3b82a6
BipinYadav1712 <bipinyadav1712@gmail.com>
2025-02-05 03:53:42 +0530

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,21 @@
+let score = "bipin"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+// NaN means not a number
+// true =>1 false => 0
+//33 => 33
+// "33abc" => NaN
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=>false
+// "" => false;
+// "bipin" => true
