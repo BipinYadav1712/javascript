112b4c2199c27463366ffb30eb8d2ecfd35caba9
BipinYadav1712 <bipinyadav1712@gmail.com>
2025-05-07 13:55:28 +0000

    comparison

---
+++ 01_basics/08_lecture8.js
@@ -0,0 +1,17 @@
+//comparison
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+
+// console.log("3">2);
+// upar aisa hota hai ki js string ko apne aap string ko int mein convert kr de rha jisese ye comparison ho paa rha
+//
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+// === isko strict check kehte hai isese datatypes bhi check karte hai
+
+console.log("3" === 2);
