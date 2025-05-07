308198bd9b14f1486daceceb6ccb9fdaa21dab73
BipinYadav1712 <bipinyadav1712@gmail.com>
2025-05-07 13:36:33 +0000

    lecture 7

---
+++ 01_basics/04_lecture7.js
@@ -0,0 +1,20 @@
+// operations in javascript
+let value = 4
+let negvalue= -value
+//console.log(negvalue);
+
+//console.log(3*3);
+//console.log(3+3);
+//console.log(3-3);
+//console.log(3**3);
+//console.log(3/3);
+
+let str1 ="hello"
+let str3 ="Bipin"
+let str2 =str1 +str3
+console.log(str2);
+
+console.log(1+3);
+console.log(1+"3");
+console.log("1"+3);
+console.log(1+3+"3");
