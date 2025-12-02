a57d3f97468a64dde717e521d9183b97be241780
BipinYadav1712 <bipinyadav1712@gmail.com>
2025-12-02 21:05:29 +0000

    maths done

---
+++ 01_basics/number&Maths.js
@@ -0,0 +1,44 @@
+// const score = 1900
+// console.log(score);
+
+// const balance = new Number(100)// explicitely bata rha ki balance number type ka hai
+// console.log(balance);
+
+// console.log(balance.toString().length); // isme balance ko string mein change kar diye hamlog
+
+// console.log(balance.toFixed(2));// toFixed ki value mostly 2 rakhi for e-commerce websites
+
+// const otherNumber = 23.48239
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString('en-IN'));
+ 
+// +++++++++++++++++ MATHS +++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-9)); // abs = absolute
+// console.log(Math.round(5.5));// mostly hamlog roundoff krte h decimal ke basis par
+// console.log(Math.ceil(5.3));// hmesha upper value dega
+// console.log(Math.floor(5.3)); // hmesha lower value dega
+// console.log(Math.sqrt(10));// squareroot dega ye
+
+// console.log(Math.min(3,5,6,1,4));
+// console.log(Math.max(3,5,6,1,4));
+
+console.log(Math.random()); /// iski value hamesha 0  aur 1 ke beech aati hai
+console.log(Math.floor(Math.random()*6)+1); // value 0 bhi ho sakti hai isiliye hamlog + 1 karte haii
+// isme hm log jo multiply kar rhe vo uski range define kar rhe
+
+const min = 10 
+const max = 20 
+console.log(Math.floor((Math.random() *(max-min+1))+min));
+
+
+
+
+
+
+
+
+
