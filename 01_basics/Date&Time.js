02ff005bfb94014965a7b9f4d195c61d9672eb50
BipinYadav1712 <bipinyadav1712@gmail.com>
2025-12-03 10:55:02 +0000

    date nd time

---
+++ 01_basics/Date&Time.js
@@ -0,0 +1,26 @@
+// DATE it is a object in java script
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(typeof myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+
+// let createdDate = new Date(2003,11,17)
+//let createdDate = new Date("2003-12-17")
+// let createdDate = new Date("07-17-2004")
+// console.log(createdDate.toDateString());
+
+// +++++++++++++ TIME STAMPS ++++++++++++++++++
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+ 
+// console.log(myDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
