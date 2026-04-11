4ff0145ee5359ac03e1d897b37ff9c8e02ee1443
BipinYadav1712 <bipinyadav1712@gmail.com>
2026-04-11 10:33:17 +0000

    api

---
+++ 09Advance/promise.js
@@ -0,0 +1,5 @@
+// promise is one of the 3 states 
+// pending: initial state neither fullfilled niether rejected
+// fullfilled : that the operation is fully completed
+// rejected : that the operation is failed 
+
